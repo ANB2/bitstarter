bcbd44f05e2e0ca95401d53fc1251f3c51eb28f6
Ubuntu <an.business2@gmail.com>
2013-08-02 22:04:39 +0000

    Updatetd web.js

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var inBuffer = fs.readFileSync('./index.html').toString('utf8'))
+var inBuffer = fs.readFileSync('index.html', 'utf8').toString('utf8');
 
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-//  console.log('Request to "/"...');
+  console.log('Request to "/"...');
 //  response.contentType('text/html' );
 //  response.send('Hello World 2 from ANB2!');
   response.send(inBuffer);
